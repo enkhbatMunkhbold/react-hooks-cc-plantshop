ed3703c9aa2f7d72814ad84fed9320ed02b7efd4
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-06-08 15:04:04 -0700

    initiate click on card function

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -8,11 +8,20 @@ function PlantCard({ plant }) {
     setInStock(prevState =>!prevState);
   }
 
+  function handleChangePrice(event) {
+    console.log(event.target.textContent);
+  }
+
+  function handleClick(event) {
+    const item = event.target === <img /> ? event.target.parentNode : event.target;;
+    console.log(item);
+  }
+
   return (
-    <li className="card" data-testid="plant-item">
+    <li className="card" data-testid="plant-item" onClick={handleClick}>
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
-      <p>Price: {plant.price}</p>
+      <p onClick={handleChangePrice}>Price: {plant.price}</p>
       {inStock ? (
         <button className="primary" onClick={handleButtonClick}>In Stock</button>
       ) : (
